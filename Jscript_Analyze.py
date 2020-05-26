26fe3c6b3ad45fb3a2c50b0da0bf183813792bc6
Devcoinfet <34756993+devcoinfet@users.noreply.github.com>
2020-05-25 21:10:45 -0700

    Update Jscript_Analyze.py

--- Jscript_Analyze.py
+++ Jscript_Analyze.py
@@ -85,7 +85,7 @@ def javascript_grabber(paths):
        pass
 
         
-def function_time(regex_str,jscript_file):
+def dom_xss_scanner(regex_str,jscript_file):
     #grab 5 lines after and before to try to get better visibility to the code affected 
     cmd =  "grep -n -C 5 "+regex_str+"  "+ jscript_file
 
@@ -121,9 +121,9 @@ def dom_xss_search(jscript_file):
             if "Property_Susceptible" in key:
                 print("trying to test sink: "+tmp_item["Property_Susceptible"])
                 try:
-                    result = function_time(tmp_item["Property_Susceptible"],jscript_file)
+                    result = dom_xss_scanner(tmp_item["Property_Susceptible"],jscript_file)
                     if result:
-                       #print(result)
+                       print(result)
                        flagged_code_hits.append(json.dumps(result))
                 except Exception as ex:
                     print(ex)
@@ -132,39 +132,45 @@ def dom_xss_search(jscript_file):
 
 
 def main():
-    try:
-       js_paths = []                 
-       results = javascript_grabber(sys.argv[1])#enter target it will give you urls of javascripts download put in dir
-       clean = list(results.split())
-       print(len(clean))
-       for url in clean:
-           print("Grabbing Javascript From URL "+str(url))
-           file_content,file_desired_path = download_javascript(url)
-           print("Desired Path "+file_desired_path)
-           beautiful_js = jsbeautifier.beautify(file_content)
-           if Path(file_desired_path).is_file():
-              print ("File exists Will Be Tested During Dom Testing")
-              js_paths.append(file_desired_path)
-           else:
-              print ("File does not exist Creating and writing Content")
-              outfile = open(file_desired_path,'w')
-              outfile.write(beautiful_js)
-              outfile.close()
-              js_paths.append(file_desired_path)
-              #now we try to create these files if they don't exist and evaluate if there are sinks
-       try:
-           for js_tests in js_paths:
-               print("Testing " +js_tests)
-               json_out = open("json_out.json","a")
-               sink_results = dom_xss_search(js_tests)  #this is used to extract the sinks this is called per file 
-               for sink in sink_results:
-                   json_out.write(json.dumps(sink)+"\n")
-               json_out.close()
+    target_list = [line.rstrip('\n') for line in open(sys.argv[1])]
+
+    for target_to_check in target_list:
+        print(target_to_check.strip())
+
+        try:
+           js_paths = []
+           results = javascript_grabber(target_to_check)#enter target it will give you urls of javascripts download put in dir
+           clean = list(results.split())
+           print(len(clean))
+           for url in clean:
+               print("Grabbing Javascript From URL "+str(url))
+               file_content,file_desired_path = download_javascript(url)
+               print("Desired Path "+file_desired_path)
+               beautiful_js = jsbeautifier.beautify(file_content)
+               if Path(file_desired_path).is_file():
+                  print ("File exists Will Be Tested During Dom Testing")
+                  js_paths.append(file_desired_path)
+               else:
+                  print ("File does not exist Creating and writing Content")
+                  outfile = open(file_desired_path,'w')
+                  outfile.write(beautiful_js)
+                  outfile.close()
+                  js_paths.append(file_desired_path)
+                  #now we try to create these files if they don't exist and evaluate if there are sinks
+           try:
+               for js_tests in js_paths:
+                   print("Testing " +js_tests)
+                   json_out = open("json_out.json","a")
+                   sink_results = dom_xss_search(js_tests)  #this is used to extract the sinks this is called per file
+                   for sink in sink_results:
+                       json_out.write(json.dumps(sink)+"\n")
+                   json_out.close()
           
-       except Exception as ohnoz:
-             print(ohnoz)
-
-    except Exception as wellnow:
-       print(wellnow)
-
+           except Exception as ohnoz:
+                print(ohnoz)
+                pass
+            
+        except Exception as wellnow:
+           print(wellnow)
+           pass
 main()
